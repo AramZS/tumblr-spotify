e0c79228f46021adec12bcbf0faba000a82c960b
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-03-08 01:52:27 -0500

    Can find track by artist and title, or popularity

--- js/home.js
+++ js/home.js
@@ -18,4 +18,39 @@ jQuery(document).ready(function() {
 			
             current.style.display = 'block';
         }
+		
+       var search = new models.Search('Rihanna');
+        search.localResults = models.LOCALSEARCHRESULTS.APPEND;
+
+        var searchHTML = document.getElementById('results');
+
+        search.observe(models.EVENT.CHANGE, function() {
+            var results = search.tracks;
+            var fragment = document.createDocumentFragment();
+			var pop = 0;
+			var mostpop = 0;
+            for (var i=0; i<results.length; i++){
+				if (results[i].name == 'Diamonds'){
+				var link = document.createElement('li');
+                var a = document.createElement('a');
+                a.href = results[i].uri;
+                link.appendChild(a);
+                a.innerHTML = results[i].name + ' where name == Diamonds';
+                fragment.appendChild(link);
+				}
+            }
+            for (var i=0; i<results.length; i++){
+				if (results[i].popularity > pop){ pop = results[i].popularity; mostpop = results[i]; }
+            }
+				var link = document.createElement('li');
+                var a = document.createElement('a');
+                a.href = mostpop.uri;
+                link.appendChild(a);
+                a.innerHTML = mostpop.name + ' where Popularity is highest';
+                fragment.appendChild(link);
+
+            searchHTML.appendChild(fragment);
+        });
+
+        search.appendNext();		
 });
